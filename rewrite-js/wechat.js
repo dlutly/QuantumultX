d97458560c027142afdd8224ef908a14f74b74e2
dlutly <77499079+dlutly@users.noreply.github.com>
2022-05-07 17:50:20 +0800

    Update wechat.js

--- rewrite-js/wechat.js
+++ rewrite-js/wechat.js
@@ -1,9 +1,11 @@
+***************************
+QuantumultX:
 [rewrite_local]
 ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/dlutly/QuantumultX/main/rewrite-js/Wechat.js
 
 [mitm]
 hostname = mp.weixin.qq.com
-
+***************************
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
